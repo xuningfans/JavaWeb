edda323b348409074da602f22178aeebf8dbe701
xuning <xuningfans@gmail.com>
2016-01-18 09:25:18 +0800

    eclipse Test

---
+++ src/main/java/HelloServlet/HelloServlet/App.java
@@ -0,0 +1,25 @@
+package HelloServlet.HelloServlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/App")
+public class App extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("HelloServlet with tomcat7 and maven!");
+		
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
